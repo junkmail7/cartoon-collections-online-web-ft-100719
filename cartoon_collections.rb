482520908e554d2fd9dcc061e495224fd589d3ce
William Pannell <lpann5@gmail.com>
2019-10-17 19:21:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,11 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   answer=""
-  i=0
+  #i=0
   x=0
-  while i!=array.size
+  #while i!=array.size
   while x!=cheese_types.size
-  if array[i].include()
+  if array.include(cheese_types[x])
+  
   return answer
 end
