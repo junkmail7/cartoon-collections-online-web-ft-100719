2e42e08c820e26a5ac85eebbc4a89495b511c386
William Pannell <lpann5@gmail.com>
2019-10-17 18:57:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.find{ |long| 
+  !!array.find{ |long| long.length>4 }
 end
 
 def find_the_cheese# code an argument here
