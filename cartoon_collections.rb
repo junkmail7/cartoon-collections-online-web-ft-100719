ecaa73778a80e352d57179896c6f6b373101c37d
William Pannell <lpann5@gmail.com>
2019-10-17 18:47:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |name| "#{name}!"
+  array.map { |name| "#{name.}!"
 end
 
 def long_planeteer_calls# code an argument here
